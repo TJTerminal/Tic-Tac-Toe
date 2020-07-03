92cf1d590d0428d79b8d38e3da1ac76ea75b9c75
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2020-07-03 14:50:00 -0700

    added functions

--- js/main.js
+++ js/main.js
@@ -26,4 +26,47 @@ let message = document.querySelector('h1');
 /*----- event listeners -----*/
 document.querySelector('table').addEventListener('click', handleMove);
 document.querySelector('button').addEventListener('click', initialize);
+
 /*----- functions -----*/
+initialize();
+
+function handleMove(evt) {
+    // obtain index of square
+    let idx = parseInt(evt.target.id.replace('sq', ''));
+    // check if square is available and return if not
+    if (board[idx] || winner) return;
+    // update state (board, turn, winner)
+    board[idx] = turn;
+    turn *= -1;
+    winner = getWinner();
+    render();
+}
+
+function getWinner() {
+    for (let i = 0; i < winningCombos.length; i++) {
+        if (Math.abs(board[winningCombos[i][0]] + board[winningCombos[i][1] + board[winningCombos[i][2]]] === 3))
+        return board[winningCombos[i][0]];
+    };
+    if (board.includes(null)) return null;
+    return 'T';
+}
+
+function render() {
+    board.forEach(function(sq, idx) {
+        squares[idx].style.background = lookup[sq];
+    });
+    if (winner === 'T') {
+        message.innerHTML = "It's a tie!"
+    } else if (winner) {
+        message.innerHTML = `Congrats ${lookup[winner].toUpperCase()}!`
+    } else {
+        message.innerHTML = `${lookup[turn].toUpperCase()}'s Turn`
+    }
+}
+
+function initialize() {
+    board = [null, null, null, null, null, null, null, null, null];
+    turn = 1;
+    winner = null;
+    render();
+}
